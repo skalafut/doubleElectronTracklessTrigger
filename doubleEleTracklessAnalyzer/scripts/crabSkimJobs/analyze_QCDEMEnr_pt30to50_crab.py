e2663f0e3ed3cb549b05c16bd287ec451afb1888
skalafut <kalaf004@umn.edu>
2017-05-09 18:41:07 +0200

    added crab3 scripts used to skim DY and QCD datasets, and added script which compresses skimmed DY dataset into trees with and without matching to GEN electrons from Z

---
+++ doubleEleTracklessAnalyzer/scripts/crabSkimJobs/analyze_QCDEMEnr_pt30to50_crab.py
@@ -0,0 +1,35 @@
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+config = config()
+
+config.General.requestName = 'skim_QCD_emenr_pt30to50'
+config.General.workArea = 'crab_project_skim_QCD_emenr_pt30to50'
+config.General.transferOutputs = True
+config.General.transferLogs = False
+
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = '/afs/cern.ch/work/s/skalafut/public/doubleElectronHLT/hlt2017/CMSSW_9_0_2/src/doubleElectronTracklessTrigger/doubleEleTracklessAnalyzer/test/hltSkimDoubleEle_QCD_emenr_pt30to50.py'
+config.JobType.maxMemoryMB = 2500 #should not need this option for skims
+
+config.Data.inputDataset = '/QCD_Pt-30to50_EMEnriched_TuneCUETP8M1_13TeV_pythia8/PhaseIFall16DR-FlatPU28to62HcalNZSRAW_90X_upgrade2017_realistic_v6_C1-v1/GEN-SIM-RAW'
+config.Data.inputDBS = 'global'
+#config.Data.splitting = 'EventAwareLumiBased'
+#use file based splitting for MC
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 15
+
+#use userInputFiles and primaryDataset when running over a list of input files which have not been published, but are available through xrootd
+#config.Data.userInputFiles = open('').readlines()
+#config.Data.primaryDataset = 'SMPL_13TeV_25ns_CHNL_skim_low_dilepton_mass_region'
+#True allows the jobs to run anywhere, regardless of where the input data is located
+#config.Data.ignoreLocality = True
+
+#totalUnits only needs to be specified for GEN-SIM jobs
+#config.Data.totalUnits = 200000
+config.Data.outLFNDirBase = '/store/group/phys_exotica/leptonsPlusJets/WR/skims/QCD_emenr_pt30to50_SKv1/'
+config.Data.publication = False
+#config.Data.publishDataName = 'skimmed'
+config.Data.outputDatasetTag = 'QCD_emenr_pt30to50'
+
+#a list of the only sites at which these jobs can run
+#config.Site.whitelist = ["T2_US*"]
+config.Site.storageSite = 'T2_CH_CERN'
